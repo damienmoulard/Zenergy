c0ceaef65ae4ebbb4223c52bbd9ce3356d4c19b5
Damien Moulard <dam.moulard@gmail.com>
2015-11-11 14:17:05 +0100

    Add Api Controllers

---
+++ Zenergy/Zenergy/Controllers/ApiControllers/usersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Zenergy.Models;
+
+namespace Zenergy.Controllers.ApiControllers
+{
+    public class usersController : ApiController
+    {
+        private ZenergyContext db = new ZenergyContext();
+
+        // GET: api/users
+        public IQueryable<user> Getuser()
+        {
+            return db.user;
+        }
+
+        // GET: api/users/5
+        [ResponseType(typeof(user))]
+        public async Task<IHttpActionResult> Getuser(int id)
+        {
+            user user = await db.user.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/users/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putuser(int id, user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.userId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/users
+        [ResponseType(typeof(user))]
+        public async Task<IHttpActionResult> Postuser(user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.user.Add(user);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.userId }, user);
+        }
+
+        // DELETE: api/users/5
+        [ResponseType(typeof(user))]
+        public async Task<IHttpActionResult> Deleteuser(int id)
+        {
+            user user = await db.user.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.user.Remove(user);
+            await db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool userExists(int id)
+        {
+            return db.user.Count(e => e.userId == id) > 0;
+        }
+    }
+}
