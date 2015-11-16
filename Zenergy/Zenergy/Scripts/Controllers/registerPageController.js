ffd0019a89138f9197807fd7609c709314d26591
Clement-Roque <clement.eberoque@gmail.com>
2015-11-16 16:18:06 +0100

    backup
    backup after VS crashes

--- Zenergy/Zenergy/Scripts/Controllers/registerPageController.js
+++ Zenergy/Zenergy/Scripts/Controllers/registerPageController.js
@@ -1,26 +1,35 @@
-﻿zenergyApp.controller("registerPageController", ["$scope", "$http", "$window", function ($scope, $http, $window) {
+﻿zenergyApp.directive('password', function () {
+
+});
+
+
+zenergyApp.controller("registerPageController", ["$scope", "$http", "$window", function ($scope, $http, $window) {
   
     $scope.user = {mail: '', password: '', passwordBis: '',  lastName:'', firstName:'', adr1:'', adr2:'', pc:'', town:'', phone:''};
     $scope.hasError = false;
-    $scope.register = function () {
-        var response = $http({
-            url: 'api/Account/register',
-            method: 'POST',
-            data: /*$httpParamSerializerJQLike(*/{ userId: 1, password: CryptoJS.MD5($scope.user.password).toString(), lastName: $scope.user.lastName, firstName: $scope.user.firstName, adr1: $scope.user.adr1, adr2: $scope.user.adr2, pc: $scope.user.pc, town: $scope.user.town, mail: $scope.user.mail, phone: $scope.user.phone }/*)*/,
-            // password: CryptoJS.MD5($scope.user.password).toString() }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function successCallback(response) {
-            $scope.hasError = false;
-           // tokenService.saveToken(response.data.access_token, response.data.userName);
-            window.location.replace("/Login");
-        }, function errorCallback(response) {
-            $scope.hasError = true;
-           // tokenService.deleteToken();
-            $scope.user.mail = '';
-            $scope.user.password = '';
-        });
+    $scope.passdiff = false;
 
-    };
+    if ($scope.user.password != $scope.user.passwordBis) {
+        $scope.passdiff = true;
+    }
+    else {
+        $scope.register = function () {
+            var response = $http({
+                url: 'api/Account/register',
+                method: 'POST',
+                data: { userId: 1, password: CryptoJS.MD5($scope.user.password).toString(), lastName: $scope.user.lastName, firstName: $scope.user.firstName, adr1: $scope.user.adr1, adr2: $scope.user.adr2, pc: $scope.user.pc, town: $scope.user.town, mail: $scope.user.mail, phone: $scope.user.phone },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                $scope.hasError = false;
+                window.location.replace("/Login");
+            }, function errorCallback(response) {
+                $scope.hasError = true;
+                $scope.user.mail = '';
+                $scope.user.password = '';
+            });
+
+        };
+    }
 }]);
