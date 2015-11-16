c5775fa511d93593f21d981a3f4c3e3ab873267b
Lyvia <lyviapavy.lp@gmail.com>
2015-11-16 14:49:38 +0100

    roomsController ok

--- Zenergy/Zenergy/Controllers/ApiControllers/roomsController.cs
+++ Zenergy/Zenergy/Controllers/ApiControllers/roomsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Zenergy.Models;
