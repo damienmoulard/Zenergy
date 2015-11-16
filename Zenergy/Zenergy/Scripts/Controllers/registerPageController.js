3c958b569de91a1b4360dcebdbfcd8ddf07c181d
Clement-Roque <clement.eberoque@gmail.com>
2015-11-16 14:29:08 +0100

    Functionnal but unsecure registration
    registration is functionnal but has to be improve and secured

--- Zenergy/Zenergy/Scripts/Controllers/registerPageController.js
+++ Zenergy/Zenergy/Scripts/Controllers/registerPageController.js
@@ -1,4 +1,4 @@
-﻿zenergyApp.controller("registerPageController",["$scope","$http", function($scope, $http){
+﻿zenergyApp.controller("registerPageController", ["$scope", "$http", "$window", function ($scope, $http, $window) {
   
     $scope.user = {mail: '', password: '', passwordBis: '',  lastName:'', firstName:'', adr1:'', adr2:'', pc:'', town:'', phone:''};
     $scope.hasError = false;
@@ -6,18 +6,18 @@
         var response = $http({
             url: 'api/Account/register',
             method: 'POST',
-            data: $httpParamSerializerJQLike({ userId: 1, password: CryptoJS.MD5($scope.user.password).toString(), lastName: $scope.user.lastName, firstName: $scope.user.firstName, adr1: $scope.user.adr1, adr2: $scope.user.adr2, pc: $scope.user.pc, town: $scope.user.town, mail: $scope.user.mail, phone: $scope.user.phone }),
+            data: /*$httpParamSerializerJQLike(*/{ userId: 1, password: CryptoJS.MD5($scope.user.password).toString(), lastName: $scope.user.lastName, firstName: $scope.user.firstName, adr1: $scope.user.adr1, adr2: $scope.user.adr2, pc: $scope.user.pc, town: $scope.user.town, mail: $scope.user.mail, phone: $scope.user.phone }/*)*/,
             // password: CryptoJS.MD5($scope.user.password).toString() }),
             headers: {
                 'Content-Type': 'application/json'
             }
         }).then(function successCallback(response) {
             $scope.hasError = false;
-            tokenService.saveToken(response.data.access_token, response.data.userName);
+           // tokenService.saveToken(response.data.access_token, response.data.userName);
             window.location.replace("/Login");
         }, function errorCallback(response) {
             $scope.hasError = true;
-            tokenService.deleteToken();
+           // tokenService.deleteToken();
             $scope.user.mail = '';
             $scope.user.password = '';
         });
