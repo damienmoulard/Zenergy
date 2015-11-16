b6efade31e5638e37881a81a8999891ede53e832
Damien Moulard <dam.moulard@gmail.com>
2015-11-16 19:36:04 +0100

    add activity controller for the api

---
+++ Zenergy/Zenergy/Controllers/ApiControllers/activitiesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Zenergy.Models;
+
+namespace Zenergy.Controllers.ApiControllers
+{
+    [Authorize]
+    public class activitiesController : ApiController
+    {
+        private ZenergyContext db = new ZenergyContext();
+
+        // GET: api/activities
+        public IQueryable<activity> Getactivity()
+        {
+            return db.activity;
+        }
+
+        // GET: api/activities/5
+        [ResponseType(typeof(activity))]
+        public async Task<IHttpActionResult> Getactivity(int id)
+        {
+            activity activity = await db.activity.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(activity);
+        }
+
+        [Authorize(Roles =("Manager"))]
+        // PUT: api/activities/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putactivity(int id, activity activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != activity.activityId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(activity).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!activityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Authorize(Roles = ("Manager"))]
+        // POST: api/activities
+        [ResponseType(typeof(activity))]
+        public async Task<IHttpActionResult> Postactivity(activity activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.activity.Add(activity);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = activity.activityId }, activity);
+        }
+
+        [Authorize(Roles = ("Manager"))]
+        // DELETE: api/activities/5
+        [ResponseType(typeof(activity))]
+        public async Task<IHttpActionResult> Deleteactivity(int id)
+        {
+            activity activity = await db.activity.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            db.activity.Remove(activity);
+            await db.SaveChangesAsync();
+
+            return Ok(activity);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool activityExists(int id)
+        {
+            return db.activity.Count(e => e.activityId == id) > 0;
+        }
+    }
+}
