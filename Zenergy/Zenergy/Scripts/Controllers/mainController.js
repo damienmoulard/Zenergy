20d98a33dfed720c3d5fb97cccaca35f5461e400
antoinejoerg <perols34>
2015-11-17 13:55:26 +0100

    Ajout page users managment

--- Zenergy/Zenergy/Scripts/Controllers/mainController.js
+++ Zenergy/Zenergy/Scripts/Controllers/mainController.js
@@ -1,7 +1,18 @@
-﻿zenergyApp.controller("mainController", ["$scope", "tokenService", "$window",  function ($scope, tokenService, $window) {
-    $scope.isConnected = tokenService.tokenExists();
+﻿zenergyApp.controller("mainController", ["$scope", "tokenService", "$window", "$resource",  function ($scope, tokenService, $window,$resource) {
+  
+
+    if (tokenService.tokenExists()) {
+        var User = $resource('api/users/:userId', { userId: '@id' });
+        //TODO : remplacer 2 par le vrai id
+        $scope.user = User.get({ userId: 2 }, function () {
+        });
+    }
+
+
     $scope.logoff = function () {
         tokenService.deleteToken();
         $window.location.reload();
+        $scope.user = null;
     };
+
 }]);
---
+++ Zenergy/Zenergy/Scripts/Controllers/usersPageController.js
@@ -0,0 +1,19 @@
+﻿zenergyApp.controller("usersPageController", ["$scope","$resource", function ($scope, $resource) {
+
+    $scope.user.$promise.then(function () {
+
+        if ($scope.user && $scope.user.admin) {
+            var User = $resource('api/users/');
+            $scope.users = User.query(function () {
+                console.log($scope.users);
+            });
+        }
+        else
+            window.location.replace("/Home");
+
+        $scope.test = function () {
+            console.log("test");
+        }
+
+    });
+}]);
