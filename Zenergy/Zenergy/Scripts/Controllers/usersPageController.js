a4e15c95abee6a9240f47dc2f34e14b7a6ae4b9f
antoinejoerg <antoine.joerg@gmail.com>
2015-11-21 14:11:01 +0100

    Add role to a user

--- Zenergy/Zenergy/Scripts/Controllers/usersPageController.js
+++ Zenergy/Zenergy/Scripts/Controllers/usersPageController.js
@@ -6,82 +6,90 @@
     $scope.sortReverse = false;  // set the default sort order
     $scope.searchFish = '';     // set the default search/filter term
 
-    $scope.users = [
-    { userId: 1, firstname: 'Crab', lastname: 'Cali', mail: 'Cali@roll.fr', administrator:false },
-    { userId: 3, firstname: 'Bob', lastname: 'Paul', mail: 'Bob@aaa@roll.fr', administrator: true },
-    { userId: 2, firstname: 'Ami', lastname: 'Pierre', mail: 'Pierre@roll.fr', administrator: true },
-    { userId: 4, firstname: 'Dude', lastname: 'Jacques', mail: 'Jacques@roll.fr', administrator: false },
-    ];
 
     var User = $resource('api/users/:userId', { userId: '@id' }, {
         update: {
             method: 'PUT' // this method issues a PUT request
         }
     });
+    var Admin = $resource('api/admins/:userId', { userId: '@id' }, {
+    });
+    var Manager = $resource('api/managers/:userId', { userId: '@id' }, {
+    });
+    var Contributor = $resource('api/contributors/:userId', { userId: '@id' }, {
+    });
+    var Member = $resource('api/members/:userId', { userId: '@id' }, {
+    });
+
 
     $scope.users = User.query(function () {
-        console.log($scope.users);
     });
-   /* $scope.checkboxModel = {
-        admins: [true,true,false,true],
-        managers: [true, true, false, true],
-        contributors: [true, true, false, true],
-        members: [true, true, false, true]
-    };
-        /*}
-        else
-            window.location.replace("/Home");*/
 
     $scope.validate = function()
     {
         angular.forEach($scope.users, function (u, key) {
 
-            if (!u.admin && u.adminChecked)
-                u.admin = { UserId: u.userId }; //admin role added
-            else if (u.admin && !u.adminChecked)
-                u.admin = null;                 //admin role removed
+            if (u.adminChecked)
+                Admin.save({userId : u.userId}) //admin role added
+            else
+                Admin.delete({ userId: u.userId }); //admin role removed
 
-            if (!u.manager && u.managerChecked)
-                u.manager = { UserId: u.userId };//manager role added
-            else if (u.manager && !u.managerChecked)
-                u.manager = null;                 //manager role removed
+            if (u.managerChecked)
+                Manager.save({ userId: u.userId });//manager role added
+            else
+                Manager.delete({ userId: u.userId }); //manager role removed
 
-            if (!u.contributor && u.contributorChecked)
-                u.contributor = { UserId: u.userId };//contributor role added
-            else if (u.contributor && !u.contributorChecked)
-                u.contributor = null;                 //contributor role removed
+            if (u.contributorChecked)
+                Contributor.save({ userId: u.userId });//contributor role added
+            else
+                Contributor.delete({ userId: u.userId }); //contributor role removed
 
-            if (!u.member && u.memberChecked)
-                u.member = { UserId: u.userId, dateMembership: new Date() };//member role added
-            else if (u.member && !u.memberChecked)
-                u.member = null;                                             //member role removed
+            if (u.memberChecked)
+                Member.save({ userId: u.userId });//member role added
+            else if (u.memberChecked)
+                Member.delete({ userId: u.userId });//member role removed
 
-            u.$update({ userId: u.userId });
         });
-        console.log($scope.users);
     }
+
+
+
 
     $scope.open = function (u) {
 
-        $scope.modifiedUser = u;
+        $scope.userTodelete = u;
 
         var modalInstance = $uibModal.open({
             animation: true,
             templateUrl: 'myModalContent.html',
             controller: 'ModalInstanceCtrl',
-            size: 'lg',
-            /*resolve: {
-                items: function () {
-                    return $scope.items;
+            size: 'sm',
+            resolve: {
+                userTodelete: function () {
+                    return $scope.userTodelete;
                 }
-            }*/
+            }
+        });
+
+        modalInstance.result.then(function () {
+            //Suppression de l'utilisateur.
+            User.delete({ userId: $scope.userTodelete.userId });
+            $('#tr' + $scope.userTodelete.userId).fadeOut('slow', function () {
+                var index = $scope.users.indexOf($scope.userTodelete);
+                $scope.users.splice(index, 1);
+            });
+
+        }, function () {
         });
     };
 
 }]);
 
-/*zenergyApp.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
-    console.log($scope.modifiedUser);
+zenergyApp.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, userTodelete) {
+
+    console.log(userTodelete);
+    $scope.userTodelete = userTodelete;
+
     $scope.ok = function () {
         $uibModalInstance.close();
     };
@@ -89,4 +97,4 @@
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
-});*/
+});
