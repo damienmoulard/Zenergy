d2475650dda708e85ae0ffab2513cb5aebc4fa98
Damien Moulard <dam.moulard@gmail.com>
2015-11-22 13:40:51 +0100

    add ponctual event ok

--- Zenergy/Zenergy/Scripts/Controllers/ponctualEventCreationPageController.js
+++ Zenergy/Zenergy/Scripts/Controllers/ponctualEventCreationPageController.js
@@ -28,6 +28,66 @@
         console.log($scope.rooms);
     });
 
-    $scope.ponctualEvent = {};
+    $scope.ponctualEvent = { eventId: 1, eventDate: new Date(), event: { eventId: 1, roomId: '', activityId: '', eventName: '', eventPrice: '', eventDurationHours: '', eventMaxPeople: '', eventDescription: '', timeBegin: '' } };
+    $scope.regular = false;
 
+    // When an activity is selected in the form
+    $scope.activitySelected = function (activity) {
+        $('#selectedActivity').attr("placeholder", activity.activityName);
+        $scope.ponctualEvent.event.activityId = activity.activityId;
+    }
+
+    // When a room is selected in the form
+    $scope.roomSelected = function (room) {
+        $('#selectedRoom').attr("placeholder", room.roomName);
+        $scope.ponctualEvent.event.roomId = room.roomId;
+    }
+
+    // If user wants to create a ponctual event
+    $scope.ponctualSelected = function () {
+        $('#ponctualSelect').attr("class", "active");
+        $('#regularSelect').attr("class", "");
+        $scope.regular = false;
+    }
+
+    $scope.regularSelected = function () {
+        $('#ponctualSelect').attr("class", "");
+        $('#regularSelect').attr("class", "active");
+        $scope.regular = true;
+    }
+
+    $scope.createEvent = function () {
+        console.log($scope.ponctualEvent);
+
+        $scope.hasError = false;
+        $scope.dateError = false;
+
+        var today = new Date();
+        if ($scope.ponctualEvent.eventDate <= today) {
+            $scope.hasError = true;
+            $scope.dateError = true;
+        }
+
+        var date = new Date($scope.ponctualEvent.event.timeBegin);
+        $scope.ponctualEvent.event.timeBegin = date.getHours().toString() + ":" + date.getMinutes().toString();
+        console.log($scope.ponctualEvent.event.timeBegin);
+        
+        if (!$scope.hasError)
+        {
+            var response = $http({
+                url: '/api/ponctualEvents',
+                method: 'POST',
+                data: $scope.ponctualEvent,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                bootbox.alert("Your event is now created!", function () {
+                    $location.path("/#/MyEvents");
+                });
+            }, function errorCallback(response) {
+                bootbox.alert("There has been an error during the event creation. Please check your event information.");
+            });
+        }
+    }
 }]);
